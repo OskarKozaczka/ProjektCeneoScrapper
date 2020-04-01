0d8380095b53c277392980f35a0e827b31b47cc5
OskarKozaczka <oskaro0@vp.pl>
2020-04-01 12:55:11 +0200

    OK 01.04

--- scraper.py
+++ scraper.py
@@ -1,14 +1,41 @@
-85910996#import bibliotek
+#import bibliotek
 import requests
 from bs4 import BeautifulSoup
 import pprint
 import json
+
+#funkcja do ekstrakcji składników opinii
+def extract_feature(opinion, selector, attribute = None):
+    try:
+        if not attribute:
+            return opinion.select(selector).pop().get_text().strip()
+        else:
+            return opinion.select(selector).pop()[attribute]
+    except IndexError:
+        return None
+
+
+#lista składowych opini wraz z selektorami i atrybutami
+selectors = {
+    "author":['div.reviewer-name-line'],
+    "recomendation":['div.product-review-summary > em'],
+    "stars":['span.review-score-count'],
+    "content": ['p.product-review-body'],
+    "pros": ['div.pros-cell > ul'],
+    "cons":['div.cons-cell > ul'],
+    "useful":['button.vote-yes',"data-total-vote"],
+    "useless":['button.vote-no',"data-total-vote"],
+    "purchased":['div.product-review-pz'],
+    "purchase_date":['span.review-time > time:nth-child(2)',"datetime"],
+    "review_date":['span.review-time > time:nth-child(1)',"datetime"]
+}
 
 #adres URL strony z opiniami
 url_prefix = "https://www.ceneo.pl"
 product_id = input("Podaj kod produktu: ")
 url_postfix = "#tab=reviews"
 url = url_prefix+"/"+product_id+url_postfix
+
 
 #pusta lista na opinie
 opinions_list = []
@@ -24,49 +51,13 @@ while url is not None:
     #ekstrakcja składowyh dla pojedynczej opinii z listy
     for opinion in opinions: 
         opinion_id = opinion["data-entry-id"]
-        author = opinion.select('div.reviewer-name-line').pop().string.strip()
-        try:
-            recomendation = opinion.select('div.product-review-summary > em').pop().string.strip()
-        except IndexError:
-            recomendation = None
-        stars = opinion.select('span.review-score-count').pop().string.strip()
-        try:
-            purchased = opinion.select('div.product-review-pz').pop().string
-        except IndexError:
-            purchased = None   
-        useful = opinion.select('button.vote-yes').pop()["data-total-vote"]
-        useless = opinion.select('button.vote-no').pop()["data-total-vote"]
-        content = opinion.select('p.product-review-body').pop().get_text().strip()
-        try:
-            cons = opinion.select('div.cons-cell > ul').pop().get_text().strip()
-        except IndexError:
-            cons = None
-        try:
-            pros = opinion.select('div.pros-cell > ul').pop().get_text().strip()
-        except IndexError:
-            pros = None
-        date = opinion.select('span.review-time > time')
-        review_date = date.pop(0)["datetime"]
-        try:
-            purchase_date = date.pop(0)["datetime"]
-        except IndexError:
-            purchase_date = None
-        
-        opinion_dict = {
-            "opinion_id":opinion_id,
-            "author":author,
-            "recomendation":recomendation,
-            "stars":stars,
-            "content": content,
-            "pros": pros,
-            "cons":cons, 
-            "useful":useful,
-            "useless":useless,
-            "purchased":purchased,
-            "purchase_date":purchase_date,
-            "review_date":review_date
-        }
-        opinions_list.append(opinion_dict)
+
+        features = {key:extract_feature(opinion, *args)
+                    for key, args in selectors.items()}
+        features["opinion_id"] = int(opinion["data-entry-id"])
+        True if features["purchased"] == "Opinia potwierdzona zakupem" else False
+
+        opinions_list.append(features)
 
     try:
         url = url_prefix+page_tree.select("a.pagination__next").pop()["href"]
@@ -77,8 +68,3 @@ while url is not None:
 
 with open(product_id+".json", 'w', encoding='utf-8') as fp:
     json.dump(opinions_list, fp, ensure_ascii=False, indent=4)
-
-
-# print(len(opinions_list))
-# for opinion in opinions_list:
-#     pprint.pprint(opinion)
